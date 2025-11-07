6728c12175c53d8145ea1da149905eab21b45fd7
RolandoCN <juan.roland@outlook.es>
2025-11-07 12:31:14 -0500

    transito3

--- app/Http/Controllers/transito/TransitoImpuestoController.php
+++ app/Http/Controllers/transito/TransitoImpuestoController.php
@@ -413,7 +413,7 @@ class TransitoImpuestoController extends Controller
             $vehiculo = TransitoVehiculo::where('id',$request->input('vehiculo_id'))->first();
             $aplica_recargo=0;
            
-            //if($vehiculo->tipo_identif=="PLACA"){
+            if($vehiculo->tipo_identif=="PLACA"){
                 $placa=$vehiculo->placa_cpn_ramv;
                 $lastChar = substr($placa, -1);
                 $mes = date("n");
@@ -425,7 +425,7 @@ class TransitoImpuestoController extends Controller
                     $aplica_recargo=1;
                 }
                 
-            //}
+            }
            
 
             $ultimo_anio_matriculacion=$request->last_year_declaracion;
