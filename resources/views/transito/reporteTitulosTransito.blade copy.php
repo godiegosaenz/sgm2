b9c8bdc4634359bdb5aa81d115321a27a4f0095c
RolandoCN <juan.roland@outlook.es>
2025-12-04 12:04:20 -0500

    indicaciones transito2

--- resources/views/transito/reporteTitulosTransito.blade copy.php
+++ resources/views/transito/reporteTitulosTransito.blade copy.php
@@ -6,7 +6,7 @@
     <title>Reporte en PDF</title>
     <style>
        @page {
-            margin-top: 3mm;  /* Ajustado */
+            margin-top: 3mm;  /* <-- antes estaba 10mm */
             margin-right: 10mm;
             margin-bottom: 10mm;
             margin-left: 10mm;
@@ -15,108 +15,75 @@
         body {
             font-family: Arial, sans-serif;
             font-size: 10px;
-            position: relative;
-            margin: 0;
-            padding: 0;
         }
-
-        /* Imagen de fondo en la mitad superior */
-        .watermark {
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 50%; /* Solo ocupa la mitad superior */
-            background-image: url('{{ asset('img/logo4.png') }}');
-            background-repeat: no-repeat;
-            background-size: contain;
-            background-position: center;
-            opacity: 0.05; /* Opacidad muy baja */
-            z-index: -1; /* La imagen queda detrás del contenido */
-        }
-
-        /* Estilos de las tablas y contenido */
         .cabecera {
             width: 100%;
             border-collapse: collapse;
             margin-bottom: 1px;
         }
-
         .cabecera td {
             vertical-align: middle;
         }
-
         .cabecera .logo {
             width: 10%;
             text-align: left;
         }
-
         .cabecera .encabezado {
             width: 90%;
             text-align: center;
             font-size: 9px;
             font-weight: bold;
         }
-
         .cabecera .subtitulo {
             font-size: 10px;
             font-weight: normal;
             margin-top: 2px;
         }
-
         .tabla-principal {
             width: 100%;
             border-collapse: collapse;
         }
-
         .tabla-principal td {
             vertical-align: top;
         }
-
         .tabla-principal .columna-izquierda {
             width: 55%;
         }
-
         .tabla-principal .columna-derecha {
             width: 45%;
         }
-
         .tabla-izquierda {
             width: 100%;
             border-collapse: collapse;
             margin-bottom: 2px;
             font-size: 11px;
         }
-
         .tabla-izquierda th, .tabla-izquierda td {
             border: none;
             padding: 4px;
             text-align: left;
         }
-
         .tabla-derecha {
             width: 100%;
             border-collapse: collapse;
             margin-bottom: 10px;
             font-size: 10px;
         }
-
         .tabla-derecha th, .tabla-derecha td {
             border: 1px solid #000;
             padding: 4px;
             text-align: left;
         }
-
         .tabla-derecha th {
             background-color: #f2f2f2;
         }
 
+      
+
     </style>
 </head>
 <body>
-    <!-- Imagen de fondo (marca de agua) -->
-    <div class="watermark"></div>
-
+   
     @php
         $sumatotal = 0;
     @endphp
@@ -124,21 +91,25 @@
     @php
         $impuesto = $item['TransitoImpuesto'];
     @endphp
-
+    
         <table class="cabecera" style="font-size:10px !important">
             <tr>
                 <td class="logo">
                     <img src="{{ asset('img/logo4.png') }}" alt="Logo" width="60">
                 </td>
                 <td class="encabezado">
+                    <!-- <h3>GAD MUNICIPAL DEL CANTÓN SAN VICENTE <h4>UNIDAD MUNICIPAL DE TRANSITO, TRANSPORTE TERRESTRE <br>Y SEGURIDAD VIAL</h4></h3> -->
+
                     <h3>GAD MUNICIPAL DEL CANTÓN SAN VICENTE <h4>DIRECCION FINANCIERA<br>
                     SAN VICENTE - MANABI - ECUADOR</h4></h3>
+                   
+
                 </td>
                  <td class="logo">
+                    <!-- <img src="{{ asset('img/logo4.png') }}" alt="Logo" width="80"> -->
                 </td>
             </tr>
         </table>
-
         <table width="100%" border="0"  style="font-size:11px">
             <tr>
                 <td colspan="3" style="text-align: left; line-height: 15px;">          
@@ -148,63 +119,83 @@
 
                 <td colspan="3" style="text-align: center;">
                     <b>TITULO DE CREDITO N° {{$impuesto->numero_titulo}}</b>
+                    <!-- San Vicente, {{ $fecha_formateada }} -->
                 </td>
+
+                
             </tr>
         </table>
         
         <table class="tabla-principal" style="margin-top: 5px;">
             <tr>
                 <td class="columna-izquierda">
-                    <table class="tabla-izquierda" style="font-size: 11px;">
+
+                    <table class="tabla-izquierda" style="font-size: 11px;">   
                         <tr>
                             <td width="50%"><b>UNIDAD MUNICIPAL DE TRANSITO, TRANSPORTE TERRESTRE <br>Y SEGURIDAD VIAL</b></td>
+                            
+                            
                         </tr>                     
                         <tr>
                             <td width="50%"><b>Proceso de Matriculacion Vehicular :</b>{{$impuesto->year_impuesto}} </td>
+                            
+                            
                         </tr>  
+                        
                         <tr>
+                            
                             <td width="50%" style=" line-height: 15px;">
                                 <b>Contribuyente :</b>{{$item['cliente']->nombres.' '.$item['cliente']->apellidos}} <br>
                                 <b>Cedula/RUC :</b>{{$item['cliente']->ci_ruc}}
                             </td>
+                            
                         </tr>  
+                        <!-- <tr>
+                          
+                            <td width="50%"><b>Cedula/RUC :</b>{{$item['cliente']->ci_ruc}} </td>
+                           
+                            
+                        </tr>  -->
+                      
                     </table>
 
                     <table class="tabla-izquierda" style="font-size: 11x;">
-                        <thead>
+                        <thead >
                             <tr>
                                 <th colspan="2">DATOS DEL VEHICULO</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr style="line-height: 10px;">
-                                <td width="30%"><strong>Placa/CPN/RAMV:</strong></td>
-                                <td width="70%">{{$item['vehiculo']->placa_cpn_ramv}}</td>
-                            </tr>
-                            <tr style="line-height: 8px;">
-                                <td width="30%"><strong>Chasis:</strong></td>
-                                <td width="70%">{{$item['vehiculo']->chasis}}</td>
-                            </tr>
-                            <tr style="line-height: 8px;">
-                                <td width="30%"><strong>Avaluo:</strong></td>
-                                <td width="70%">${{number_format($item['vehiculo']->avaluo,2)}}  </td>
-                            </tr>
-                            <tr style="line-height: 8px;">
-                                <td width="30%"><strong>Año de modelo:</strong></td>
-                                <td width="70%">{{$item['vehiculo']->year}}</td>
-                            </tr>
-                            <tr style="line-height: 8px;">
-                                <td width="30%"><strong>Clase tipo:</strong></td>
-                                <td width="70%">{{$item['vehiculo']->clase_vehiculo->descripcion}}</td>
-                            </tr>
-                            <tr style="line-height: 8px;">
-                                <td width="30%"><strong>Marca:</strong></td>
-                                <td width="70%">{{$item['vehiculo']->marca->descripcion}}</td>
-                            </tr>
-                            <tr style="line-height: 8px;">
-                                <td width="30%"><strong>Tipo Servicio:</strong></td>
-                                <td width="70%">{{$item['vehiculo']->tipo_vehi ?? 'PUBLICO'}}</td>
-                            </tr>
+
+                                <tr style="line-height: 10px;">
+                                    <td width="30%"><strong>Placa/CPN/RAMV:</strong></td>
+                                    <td width="70%">{{$item['vehiculo']->placa_cpn_ramv}}</td>
+                                </tr>
+                                <tr style="line-height: 8px;">
+                                    <td width="30%"><strong>Chasis:</strong></td>
+                                    <td width="70%">{{$item['vehiculo']->chasis}}</td>
+                                </tr>
+                                <tr style="line-height: 8px;">
+                                    <td width="30%"><strong>Avaluo:</strong></td>
+                                    <td width="70%">${{number_format($item['vehiculo']->avaluo,2)}}  </td>
+                                </tr>
+                                <tr style="line-height: 8px;">
+                                    <td width="30%"><strong>Año de modelo:</strong></td>
+                                    <td width="70%">{{$item['vehiculo']->year}}</td>
+                                </tr>
+                                <tr style="line-height: 8px;">
+                                    <td width="30%"><strong>Clase tipo:</strong></td>
+                                    <td width="70%">{{$item['vehiculo']->clase_vehiculo->descripcion}}</td>
+                                </tr>
+                                <tr style="line-height: 8px;">
+                                    <td width="30%"><strong>Marca:</strong></td>
+                                    <td width="70%">{{$item['vehiculo']->marca->descripcion}}</td>
+                                </tr>
+                                <tr style="line-height: 8px;">
+                                    <td width="30%"><strong>Tipo Servicio:</strong></td>
+                                    <td width="70%">{{$item['vehiculo']->tipo_vehi ?? 'PUBLICO'}}</td>
+                                </tr>
+
                         </tbody>
                     </table>
 
@@ -223,6 +214,7 @@
                                     $concepto=$r->concepto;
                                     $quitar=0;
                                     if($r->codigo=='RTV'){
+                                        //$concepto=$r->concepto ." (".implode(', ', $r->agrupado). ")";
                                         $concepto=$r->concepto ." (".$item['vehiculo']->tipo_vehiculo->descripcion. ")";
                                     }
 
@@ -230,32 +222,40 @@
                                         $concepto=$r->concepto ." (CALENDARIZACION ".$impuesto['calendarizacion'].")";
                                     }
 
-
+                                    /*if($concepto=="RECARGO POR CALENDARIZACIÓN" && $r->pivot->valor==0){
+                                        $quitar=1;
+                                    }*/
                                 @endphp
-                                
+                                <!-- @if($quitar==0) -->
                                     <tr>
                                         <td>
                                             {{$concepto}}
                                         </td>
                                         <td style="text-align: right; vertical-align:middle">$ {{$r->pivot->valor}}</td>
-                                    </tr>                             
-                                    
+                                    </tr>
+                                <!-- @endif -->
                             @endforeach
+
                         </tbody>
                         <tfoot>
                             <tr>
                                 <td><STRONg>TOTAL A PAGAR</STRONg> </td>
-                                <td style="text-align: right;"><STRONg>$ {{$impuesto->total_pagar}}</STRonG> </td>
+                                <td style="text-align: right;"><STRONg>$ {{$impuesto->total_pagar}}</STRONg> </td>
                             </tr>
                         </tfoot>
                     </table>
+                   
                 </td>
             </tr>
         </table>
-
+       
+        @php
+            $sumatotal = 10
+        @endphp
     @endforeach
 
-      <table width="100%" style="margin-top:0px">
+
+    <table width="100%" style="margin-top:0px">
         <tbody>
 
             <tr style="line-height: 5px;">
@@ -309,5 +309,6 @@
             </tr>
         </tbody>
     </table>
+                                  
 </body>
 </html>
