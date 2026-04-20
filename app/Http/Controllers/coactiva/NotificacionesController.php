d0d65c3a1382d491f3102980d7a12a7410f52551
RolandoCN <juan.roland@outlook.es>
2026-04-20 13:20:33 -0500

    coactiva mejoras12

--- app/Http/Controllers/coactiva/NotificacionesController.php
+++ app/Http/Controllers/coactiva/NotificacionesController.php
@@ -1006,7 +1006,7 @@ class NotificacionesController extends Controller
             ini_set("memory_limit",-1);
             ini_set('max_execution_time', 0);
 
-            //*****VALIDACIONES RESPECTIVAS*******************************************/
+            //*****VALIDACIONES RESPECTIVAS*********************************************/
 
             $noti=InfoNotifica::where('estado','Notificado')
             ->where('id',$id)
