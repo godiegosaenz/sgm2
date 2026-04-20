b0c91b09e47e2d2f64ed4eb75a48bcaca1ca9da0
RolandoCN <juan.roland@outlook.es>
2026-04-20 13:06:52 -0500

    coactiva mejoras12

--- app/Http/Controllers/coactiva/NotificacionesController.php
+++ app/Http/Controllers/coactiva/NotificacionesController.php
@@ -1002,6 +1002,9 @@ class NotificacionesController extends Controller
         DB::connection('pgsql')->beginTransaction();
 
         try{
+            set_time_limit(0);
+            ini_set("memory_limit",-1);
+            ini_set('max_execution_time', 0);
 
             //*****VALIDACIONES RESPECTIVAS*******************************************/
 
