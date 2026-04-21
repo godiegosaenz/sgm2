577f194938394d713163c8914f919625909b0910
RolandoCN <juan.roland@outlook.es>
2026-04-21 16:47:08 -0500

    parte diario transito ordenar asc listado reporte

--- app/Http/Controllers/analitica/AnaliticaContribuyenteController.php
+++ app/Http/Controllers/analitica/AnaliticaContribuyenteController.php
@@ -254,7 +254,7 @@ class AnaliticaContribuyenteController extends Controller
             ->whereBetween('i.fecha_cobro', [$desde, $hasta])
             ->where('i.estado',3)
             ->select('v.placa_cpn_ramv','v.chasis','v.avaluo','v.year','mv.descripcion as marca_veh','cv.descripcion as clase','en.nombres as nombre_propietario','en.apellidos as apellido_propietario','en.ci_ruc as identificacion_propietario' ,'i.year_impuesto','i.numero_titulo','i.total_pagar','i.usuario','i.created_at','i.id as identificador','clv.descripcion as clase_desc','i.id_usuario_cobra','i.fecha_cobro')
-            ->orderBy('i.id','desc')
+            ->orderBy('i.id','asc')
             ->get();
 
             foreach($consultar as $key=> $data){
@@ -393,9 +393,7 @@ class AnaliticaContribuyenteController extends Controller
                 return (object) $item;
             });
 
-            // foreach ($datosPagos as $key => $data) {
-            //     dd($data->concepto);
-            // }
+           
 
             $nombrePDF="reporte_pago_transito.pdf";
 
